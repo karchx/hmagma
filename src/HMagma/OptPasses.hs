f6bb27b4b3e8dd6ca4248bec36475eea2eb981ad
karchx <kenethra.74@gmail.com>
2026-09-11 13:33:02 -0600

    feat: add rewriteOp

--- src/HMagma/OptPasses.hs
+++ src/HMagma/OptPasses.hs
@@ -7,26 +7,30 @@ import qualified Data.Map.Strict as M
 import Data.Map.Strict (Map)
 
 type VNTable = Map (BinaryOp, Operand, Operand) Temp
+type VNTableRename = Map Temp Temp
 
-vnRename :: Map Temp Temp
-vnRename = M.empty
+
+rewriteOp :: VNTableRename -> Operand -> Operand
+rewriteOp r (OTemp t) = OTemp (M.findWithDefault t t r)
+rewriteOp _ o         = o
 
 localValueNumbering :: [TAC] -> [TAC]
-localValueNumbering = go M.empty
+localValueNumbering = go M.empty M.empty
     where
-        go :: VNTable -> [TAC] -> [TAC]
-        go _ [] = []
-        go vn (i:is) = case i of
+        go :: VNTable -> VNTableRename -> [TAC] -> [TAC]
+        go _ _ [] = []
+        go vn rename (i:is) = case i of
             TBinOp dest op a b ->
                 let key = (op, a, b)
                 in case M.lookup key vn of
                     Just old ->
                         -- ignore redundancy code
                         -- and save register for rename redundancy
-                        let _ = M.insert old (Temp 2) vnRename
-                        in go vn is
+                        let rename' = M.insert dest old rename
+                        in go vn rename' is
                     Nothing ->
                         let vn' = M.insert key dest vn
-                        in TBinOp dest op a b : go vn' is
+                        in TBinOp dest op a b : go vn' rename is
             TAssign dest src ->
-                TAssign dest src : go vn is
+                let src' = rewriteOp rename src
+                in TAssign dest src' : go vn rename is
